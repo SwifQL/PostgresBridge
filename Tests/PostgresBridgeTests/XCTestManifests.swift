507afea97ca62a5e75ca6c013792fe8145eade95
Mihael Isaev <isaev.mihael@gmail.com>
2020-01-30 03:19:27 +0400

    Fix failing tests

--- Tests/PostgresBridgeTests/XCTestManifests.swift
+++ Tests/PostgresBridgeTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SwifQLORMTests.allTests),
+        testCase(PostgresBridgeTests.allTests),
     ]
 }
 #endif
